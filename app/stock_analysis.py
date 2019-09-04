10a0e6d9ca66566c76d4c901eae4eb91fcbff47f
OneZero Binary LTD <info@onezerobinary.com>
2019-09-04 10:11:56 +0000

    update stock code

--- app/stock_analysis.py
+++ app/stock_analysis.py
@@ -2,6 +2,9 @@ import pandas as pd
 import datetime
 import pandas_datareader.data as web
 from pandas import DataFrame
+import matplotlib.pyplot as plt
+from matplotlib import style
+import matplotlib as mpl
 
 
 start = datetime.datetime(2019, 1, 1)
@@ -10,5 +13,20 @@ end = datetime.datetime(2019, 9, 3)
 df = web.DataReader("BTC-USD", 'yahoo', start, end)
 print(df.tail())
 
-# Determine trend using Moving Average and remove "noises".
 
+# Determine trend using Moving Average and remove "noises".
+final_close_price = df['Adj Close']
+last_days = 150
+moving_avarage = final_close_price.rolling(window=last_days).mean()
+# print(moving_avarage)
+
+# Adjust the size of mathplotlib
+mpl.rc('figure', figsize=(8, 7))
+mpl.__version__
+
+# Adjust the style of matplotlib
+style.use('ggplot')
+
+final_close_price.plot(label='BTC-USD')
+moving_avarage.plot(label='moving average')
+plt.legend()
--- app/tweet_sentiment.py
+++ app/tweet_sentiment.py
@@ -18,14 +18,13 @@ auth.set_access_token(access_token_key, access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True)
 # api = tweepy.API(auth)
 topicname = os.getenv("TOPIC_NAME", "enrico zanardo")
-public_tweets = tweepy.Cursor(api.search, q=topicname, count=10, lang="en", since="2019-01-01").items()
+public_tweets = tweepy.Cursor(api.search, q=topicname, lang="en", since="2019-01-01").items(10)
 unwanted_words = ['@', 'RT', ':', 'https', 'http']
 symbols = ['@', '#']
 data = []
 
 for tweet in public_tweets:
-    text = TextBlob(tweet.text.encode('utf-8').lower())
-    text.correct()
+    text = tweet.text.encode('utf-8').lower()
     text_words = text.split()
     # print(text_words)
     cleaned_tweet = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)|(RT)", " ", text).split())
